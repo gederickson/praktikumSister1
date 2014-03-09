b3c85974bcdd9f3d5cf6d6fcfa998532141dfc1d
madedia <dia.agustya@live.com>
2014-03-10 00:25:34 +0700

    Modify client socket

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -6,6 +6,9 @@
 
 package client;
 
+import java.io.*;
+import java.net.*;
+
 /**
  *
  * @author MD Dia Agustya
@@ -17,6 +20,33 @@ public class Client {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            Socket clientSocket;
+            DataInputStream inputStream;
+            DataOutputStream outputStream;
+            String message;
+        
+            /*
+                Buat socket, output stream, kirim pesan
+            */
+            clientSocket = new Socket("localhost", 5111);
+            outputStream = new DataOutputStream(clientSocket.getOutputStream());
+            outputStream.writeUTF("Message GUEST");
+            
+            /*
+             * preparing data input stream, receive message from server, print
+             */
+            inputStream = new DataInputStream(clientSocket.getInputStream());
+            message = inputStream.readUTF();
+            System.out.println("From server: " + message);
+            
+        }
+        catch(IOException e) {
+            System.out.println("IO exception: " + e.getMessage());
+        }
+        
+        
+        
     }
     
 }
