785603bc0d33f799db7f36e682667f5a2413fcf9
madedia <dia.agustya@live.com>
2014-03-10 00:42:11 +0700

    Modify socket in Server

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -6,6 +6,8 @@
 
 package server;
 
+import java.net.*;
+import java.io.*;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -22,6 +24,45 @@ public class Server {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            /*
+             * variable initialization
+             */
+            ServerSocket serverSocket;            
+            Socket clientSocket;
+            DataInputStream inputStream;
+            DataOutputStream outputStream;
+            String message;
+                        
+            /*
+             * create socket server, accept client, preparing input stream
+             * receive message, and print to screen
+             */
+            serverSocket = new ServerSocket(5111);
+            clientSocket = serverSocket.accept();
+            inputStream = new DataInputStream(clientSocket.getInputStream());
+            message = inputStream.readUTF();
+            System.out.println("From client: " + message);
+
+            /*
+             * preparing output stream, send message back to client
+             */
+            outputStream = new DataOutputStream(clientSocket.getOutputStream());
+            outputStream.writeUTF(message);
+
+            /*
+             * close input stream, output stream
+             * close client socket and server socket
+             */
+            inputStream.close();
+            outputStream.close();
+            clientSocket.close();
+            serverSocket.close();
+        }
+        catch(IOException e) {
+            System.out.println("Listen: " + e.getMessage());
+        }        
+        
         Reader file = new Reader();
         file.readDay("Kamis");
  
