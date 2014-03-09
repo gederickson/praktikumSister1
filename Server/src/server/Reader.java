a8c431b084ef8a9b2ea32fecb1c09f98e273c4d6
gederickson <gederickson@yahoo.co.id>
2014-03-09 23:49:31 +0700

    Modify Reader & Server
    Read all & read by day success

--- Server/src/server/Reader.java
+++ Server/src/server/Reader.java
@@ -6,43 +6,53 @@
 
 package server;
  
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author Rickson
  */
-public class Reader {
-
- 
-public void readAll() 
-    {
-        Reader file = new Reader();
-	BufferedReader br = null;
- 	try 
-            {
-		String sCurrentLine;
-		br = new BufferedReader(new FileReader("file.txt"));
-		while ((sCurrentLine = br.readLine()) != null) 
-                    {
-                        System.out.println(sCurrentLine);
-                    }
-            } 
-                catch (IOException e) 
-                {
-			e.printStackTrace();
-		} 
-            finally 
-            {
-		try 
-                {
-			if (br != null)br.close();
-		} 
-                catch (IOException ex) 
-                {
-			ex.printStackTrace();
-		}
-            }
+public class Reader 
+{
+public static String initialRead() throws FileNotFoundException {
+     //Z means: "The end of the input but for the final terminator, if any"
+    String temp;    
+    String output = new Scanner(new File("file.txt")).useDelimiter("\\Z").next();
+        temp="" + output;
+    return temp;
     }
+    
+    
+ public void readAll() throws FileNotFoundException
+    {
+        String temp;
+        temp = initialRead();
+        System.out.println(temp);
+    }
+
+ public void bacaHari( String hari) throws FileNotFoundException
+    {
+        String temp;
+        int i;
+        temp = initialRead();
+        String perBaris [] = temp.split("\n");
+        System.out.println(perBaris[0]);
+        for(i=0;i<perBaris.length;i++)
+        {
+            if(perBaris[i].indexOf(hari) != -1)
+            {
+                System.out.println(perBaris[i]);
+            }
+        }
+    }
+
+
+    
+    
 }
--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -6,7 +6,9 @@
 
 package server;
 
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.String;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -19,11 +21,10 @@ public class Server {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
         Reader file = new Reader();
-        file.readAll();
- 
+        file.bacaHari("Kamis");
 	}
     }
     
