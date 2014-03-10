ccdbfa5976299ee2a8b373853dbf695c13315624
Made Dia Agustya <dia.agustya@live.com>
2014-03-10 12:29:43 +0700

    rebuild project Message
    Project sebelumnya terhapus

---
+++ MessageSerialization/src/messageserialization/Message.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package messageserialization;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author MD Dia Agustya
+ */
+public class Message implements Serializable {
+    private String message;
+    
+    public Message(String message) {
+        this.message = message;
+    }
+    
+    public String getString() {
+        return message;
+    }
+}
